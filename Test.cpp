3cf72256df9adef93a218355a75ddb532fc1fb52
Maxwell <maxwellrocker@gmail.com>
2011-10-18 15:41:32 +0800

    update

--- Test.cpp
+++ Test.cpp
@@ -1,9 +1,18 @@
 #include "Test.h"
 #include<stdio.h>
+#include<string>
 extern "C"{
     #include "lua.h"
     #include "lualib.h"
     #include "lauxlib.h"
+}
+
+static int SayHello(lua_State* L){
+    const char* Name = lua_tostring(L, 1);
+    std::string Hello = "Hello! ";
+    Hello += Name;
+    lua_pushstring(L, Hello.c_str());
+    return 1;
 }
 
 int main()
@@ -14,7 +23,16 @@ int main()
 
     lua_State* L = luaL_newstate();
 	luaL_openlibs(L);
+	lua_register(L, "SayHello", SayHello);
 	luaL_dofile(L, "script.lua");
+
+    /**/
+	lua_getglobal(L, "plus");
+	lua_pushnumber(L, 10);
+	lua_pushnumber(L, 5);
+	lua_pcall(L, 2, 1, 0);
+	printf("%d", (int)lua_tonumber(L, -1));
+	lua_pop(L, 1);
 
 	char buff[256];
 	while(fgets(buff, sizeof(buff), stdin) != NULL)
@@ -26,6 +44,8 @@ int main()
 
     return 0;
 }
+
+
 
 /*
 int main(void)
---
+++ script.lua
@@ -0,0 +1,7 @@
+print("This is loaded from 'script.lua'");
+
+print(SayHello("Hugh Jackman"));
+
+function plus(x, y)
+	return (x+y)
+end
